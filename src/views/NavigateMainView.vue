7bc71e9f282184d6fe2ea1722adad50db4edcb46
gegr3 <greg.ge@oocl.com>
2020-12-01 09:59:14 +0800

    update NavigateMainView

--- src/views/NavigateMainView.vue
+++ src/views/NavigateMainView.vue
@@ -1,54 +1,32 @@
 <template>
-  <!--      <div class="main-container">-->
-  <!--        <div-->
-  <!--          class="link-div"-->
-  <!--          @click="linkView"-->
-  <!--          :key="item.id"-->
-  <!--          v-for="(item, index) in normalRouterData">-->
-  <!--          <router-link :id="item.id" :to="item.to">{{ item.text || index }}</router-link>-->
-  <!--        </div>-->
-  <!--      </div>-->
-  <div class="main-container">
-    <el-row :gutter="20" type="flex" justify="space-around" align="center">
-      <el-col :span="4" :key="item.id" v-for="item in normalRouterData">
-        <div class="navigate-link-div" :class="item.itemClass">
-          <router-link :id="item.id" :to="item.to">{{ item.text }}</router-link>
+      <div class="main-container">
+        <div
+          class="link-div"
+          @click="linkView"
+          :key="item.id"
+          v-for="(item, index) in routerData">
+          <router-link :id="item.id" :to="item.to">{{ item.text || index }}</router-link>
         </div>
-      </el-col>
-    </el-row>
-    <el-row :gutter="20" type="flex" justify="space-around">
-      <el-col :span="4" :key="item.id" v-for="item in singlePageRouterData">
-        <a class="navigate-link-div navigate-item-specialLevel" :href="item.to">{{ item.text }}</a>
-      </el-col>
-    </el-row>
-  </div>
+      </div>
 </template>
 
 <script>
 export default {
   name: 'NavigateMainView',
   data () {
-    let normalRouterData = []
+    let routers = []
     Array.apply(1, {length: 5}).forEach((v, i) => {
-      normalRouterData.push({
+      routers.push({
         id: 'link' + i,
-        to: `index/page${i}`,
-        text: i,
-        itemClass: (i % 2) === 0 ? 'navigate-item-levelA' : 'navigate-item-levelB'
+        to: `index/page${i}`
       })
     })
     return {
-      normalRouterData: [{
-        id: Math.random(),
-        to: '',
-        text: '',
-        itemClass: (Math.random() * 10 % 2) === 0 ? 'navigate-item-levelA' : 'navigate-item-levelB'
-      }, ...normalRouterData],
-      singlePageRouterData: [{
+      routerData: [{
         id: 'linkSpecial-1',
-        to: './singlepageview/Page1',
-        text: 'singlepageview/Page1'
-      }]
+        to: 'Index',
+        text: 'Index/PageA'
+      }, ...routers]
     }
   },
   methods: {
@@ -63,57 +41,28 @@ export default {
 </script>
 
 <style scoped>
-/*.main-container {*/
-/*  box-sizing: border-box;*/
-/*  height: 100%;*/
-/*  width: 100%;*/
-/*  padding: 100px 200px;*/
-/*  display: flex;*/
-/*  justify-content: space-around;*/
-/*  align-items: center;*/
-/*  align-content: space-around;*/
-/*}*/
-
-/*.main-container > div {*/
-/*  min-width: 80px;*/
-/*  padding: 0 10px;*/
-/*  height: 100px;*/
-/*  background-color: #42b983;*/
-/*  line-height: 100px;*/
-/*  text-align: center;*/
-/*  font-size: 40px;*/
-/*}*/
-
-/*.main-container > div > a {*/
-/*  text-decoration: none;*/
-/*}*/
 .main-container {
-  height: 100vh;
-  width: 100vw;
+  box-sizing: border-box;
+  height: 100%;
+  width: 100%;
+  padding: 100px 200px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  align-content: space-around;
 }
 
-.main-container > .el-row {
-  margin-bottom: 1rem;
-  /*height: 30vh;*/
-}
-
-.navigate-link-div {
-  min-height: 1rem;
+.main-container > div {
+  min-width: 80px;
+  padding: 0 10px;
+  height: 100px;
   background-color: #42b983;
-  border-radius: 0.5rem;
-  padding: 0.5rem 1rem;
+  line-height: 100px;
   text-align: center;
+  font-size: 40px;
 }
 
-.navigate-item-specialLevel {
-  background-color: #d2f3e4;
-}
-
-.navigate-item-levelA {
-  background-color: #42b983;
-}
-
-.navigate-item-levelB {
-  background-color: #42b900;
+.main-container > div > a {
+  text-decoration: none;
 }
 </style>

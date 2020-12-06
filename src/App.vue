5d39574afff2ed5fa82bfb6be373011b4f78bb5c
zhuge9527 <gzs9527@126.com>
2020-12-06 17:16:44 +0800

    fix sass-loader and complete snow-background

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ html, body, #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: #000000;
   display: flex;
   justify-content: center;
   align-items: center;
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,6 @@
 <template>
   <el-container style="flex-grow: 0; text-align: center">
+    <div id="snow-div-list"></div>
     <el-header>
       <h1>Welcome To My Website (Test Version)</h1>
     </el-header>
@@ -34,7 +35,6 @@
           </el-button>
         </el-col>
       </el-row>
-      <div id="snow-div-list"></div>
     </el-main>
     <el-footer>
       <span>The main technologies used in this project are: Vue, Vuex, Vue Router, ElementUI ...</span>
@@ -75,7 +75,7 @@ export default {
             class: 'snow-item'
           }))
         }
-        return h('div', {id: 'snow-div-list'}, snowDomList)
+        return h('div', {class: 'snow-div-list'}, snowDomList)
       }
     })
     let background = new SnowBackground()
@@ -83,64 +83,68 @@ export default {
   }
 }
 </script>
-<!--<style scoped lang="scss">-->
-<!--.login-view-main-form {-->
-<!--  margin: 10% 0 30% 0;-->
-<!--  height: 200px;-->
-<!--  padding: 20px 140px;-->
-<!--  display: flex;-->
-<!--  flex-direction: column;-->
-<!--  justify-content: space-between;-->
-<!--}-->
+<style lang="scss">
+.login-view-main-form {
+  margin: 10% 0 30% 0;
+  height: 200px;
+  padding: 20px 140px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+.snow-div-list {
+  position: fixed;
+  left: 0;
+  top: 0;
+  filter: drop-shadow(0 0 10px white);
+}
+body {
+  height: 100vh;
+  background: radial-gradient(ellipse at bottom, #5e6063 0%, #0e1744 100%);
+  overflow: hidden;
+}
 
-<!--body {-->
-<!--  height: 100vh;-->
-<!--  background: radial-gradient(ellipse at bottom, #1b2735 0%, #090a0f 100%);-->
-<!--  overflow: hidden;-->
-<!--  filter: drop-shadow(0 0 10px white);-->
-<!--}-->
+@function random_range($min, $max) {
+  $rand: random();
+  $random_range: $min + floor($rand * (($max - $min) + 1));
+  @return $random_range;
+}
 
-<!--@function random_range($min, $max) {-->
-<!--  $rand: random();-->
-<!--  $random_range: $min + floor($rand * (($max - $min) + 1));-->
-<!--  @return $random_range;-->
-<!--}-->
+.snow-item {
+  $total: 200;
+  position: absolute;
+  width: 10px;
+  height: 10px;
+  background: white;
+  border-radius: 50%;
 
-<!--.snow {-->
-<!--  $total: 200;-->
-<!--  position: absolute;-->
-<!--  width: 10px;-->
-<!--  height: 10px;-->
-<!--  background: white;-->
-<!--  border-radius: 50%;-->
+  @for $i from 1 through $total {
+    $random-x: random(1000000) * 0.0001vw;
+    $random-offset: random_range(-100000, 100000) * 0.0001vw;
+    $random-x-end: $random-x + $random-offset;
+    $random-x-end-yoyo: $random-x + ($random-offset / 2);
+    $random-yoyo-time: random_range(30000, 80000) / 100000;
+    $random-yoyo-y: $random-yoyo-time * 100vh;
+    $random-scale: random(10000) * 0.0001;
+    $fall-duration: random_range(10, 30) * 1s;
+    $fall-delay: random(30) * -1s;
 
-<!--  @for $i from 1 through $total {-->
-<!--    $random-x: random(1000000) * 0.0001vw;-->
-<!--    $random-offset: random_range(-100000, 100000) * 0.0001vw;-->
-<!--    $random-x-end: $random-x + $random-offset;-->
-<!--    $random-x-end-yoyo: $random-x + ($random-offset / 2);-->
-<!--    $random-yoyo-time: random_range(30000, 80000) / 100000;-->
-<!--    $random-yoyo-y: $random-yoyo-time * 100vh;-->
-<!--    $random-scale: random(10000) * 0.0001;-->
-<!--    $fall-duration: random_range(10, 30) * 1s;-->
-<!--    $fall-delay: random(30) * -1s;-->
+    &:nth-child(#{$i}) {
+      opacity: random(10000) * 0.0001;
+      transform: translate($random-x, -10px) scale($random-scale);
+      animation: fall-#{$i} $fall-duration $fall-delay linear infinite;
+    }
 
-<!--    &:nth-child(#{$i}) {-->
-<!--      opacity: random(10000) * 0.0001;-->
-<!--      transform: translate($random-x, -10px) scale($random-scale);-->
-<!--      animation: fall-#{$i} $fall-duration $fall-delay linear infinite;-->
-<!--    }-->
+    @keyframes fall-#{$i} {
+      #{percentage($random-yoyo-time)} {
+        transform: translate($random-x-end, $random-yoyo-y) scale($random-scale);
+      }
 
-<!--    @keyframes fall-#{$i} {-->
-<!--      #{percentage($random-yoyo-time)} {-->
-<!--        transform: translate($random-x-end, $random-yoyo-y) scale($random-scale);-->
-<!--      }-->
+      to {
+        transform: translate($random-x-end-yoyo, 100vh) scale($random-scale);
+      }
+    }
+  }
+}
 
-<!--      to {-->
-<!--        transform: translate($random-x-end-yoyo, 100vh) scale($random-scale);-->
-<!--      }-->
-<!--    }-->
-<!--  }-->
-<!--}-->
-
-<!--</style>-->
+</style>
